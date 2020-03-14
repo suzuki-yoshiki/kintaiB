00bd4a2e59f681466eee1fbc8dcde22c45de0929
suzuki yoshiki <yoshisu19940418@yahoo.co.jp>
2020-03-14 04:13:07 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,60 +3,65 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
   
-  def index
-    @users = User.paginate(page: params[:page])
-  end
   
-  def show
-  end
-  
-  def new
-    @user = User.new
-  end
-  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in @user # 保存成功後、ログインします。
-      flash[:success] = "新規作成に成功しました。"
-      redirect_to @user
-    else
-      render :new
+    def index
+      @users = User.paginate(page: params[:page])
     end
-  end
-  
-  def edit
-  end
-  
-  def update
-    if @user.update_attributes(user_params)
-       flash[:success] = "ユーザー情報を更新しました。"
-       redirect_to @user# 更新に成功した場合の処理を記述します。
-    else
-       render :edit      
+    
+    def show
+      @first_day = Date.current.beginning_of_month
+      @last_day = @first_day.end_of_month
+      @worked_sum = @attendances.where.not(started_at: nil).count
     end
-  end
-  
-  def destroy
-    @user.destroy
-    flash[:success] = "#{@user.name}のデータを削除しました。"
-    redirect_to users_url
-  end
-  
-  def edit_basic_info
-  end
-
-  def update_basic_info
-    if @user.update_attributes(basic_info_params)
-     # 更新成功時の処理
-     flash[:success] = "#{@user.name}の基本情報を更新しました。"
-    else
-     # 更新失敗時の処理
-     flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    
+    def new
+      @user = User.new
     end
-     redirect_to users_url
-  end
+    
+    def create
+        @user = User.new(user_params)
+      if @user.save
+        log_in @user # 保存成功後、ログインします。
+        flash[:success] = "新規作成に成功しました。"
+        redirect_to @user
+      else
+        render :new
+      end
+    end
+    
+    def edit
+    end
+    
+    def update
+      if @user.update_attributes(user_params)
+         flash[:success] = "ユーザー情報を更新しました。"
+         redirect_to @user# 更新に成功した場合の処理を記述します。
+      else
+         render :edit      
+      end
+    end
+    
+    def destroy
+      @user.destroy
+      flash[:success] = "#{@user.name}のデータを削除しました。"
+      redirect_to users_url
+    end
+    
+    def edit_basic_info
+    end
+  
+    def update_basic_info
+      if @user.update_attributes(basic_info_params)
+       # 更新成功時の処理
+       flash[:success] = "#{@user.name}の基本情報を更新しました。"
+      else
+       # 更新失敗時の処理
+       flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+      end
+       redirect_to users_url
+    end
   
   private
 
@@ -89,9 +94,8 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
     
-    # システム管理権限所有かどうか判定します。
+      # システム管理権限所有かどうか判定します。
     def admin_user
-      redirect_to root_url unless current_user.admin?
+          redirect_to root_url unless current_user.admin?
     end
-    
 end
